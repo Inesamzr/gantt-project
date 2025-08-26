b3345c2933760b24aa2abf2940dee1019e32a3a1
ines.amzert <ines.amzert@codein.fr>
2025-08-26 16:43:27 +0200

    feat(app): implement main layout structure with header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="min-h-screen flex flex-col bg-[#DEE5EC] overflow-x-hidden">
+      {/* Header avec logo */}
+      <header className="p-4 pl-8">
+        <img src="/logo.svg" alt="Clayverest" className="h-10" />
+      </header>
+
+      {/* Contenu principal */}
+      <main className="flex-1 px-8 py-4"></main>
+
+      {/* Pied de page avec montagne */}
+      <footer className="relative w-full">
+        <img
+          src="/Everest_Gris.svg"
+          alt="Montagne"
+          className="absolute bottom-0 left-0 w-full object-cover opacity-50"
+        />
+      </footer>
+    </div>
+  );
 }
 
 export default App;
