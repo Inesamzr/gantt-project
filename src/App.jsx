a6448ef42a4fe5331924ee4981890ccc5eca0663
ines.amzert <ines.amzert@codein.fr>
2025-08-28 14:54:43 +0200

    feat(app): enhance layout with gradient background and theme variables

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-[#DEE5EC] overflow-x-hidden">
-      {/* Header avec logo */}
+    <div className="min-h-screen flex flex-col overflow-x-hidden bg-gradient-to-t from-primary-blue/20 to-background-page">
       <header className="p-4 pl-8">
         <img src="/logo.svg" alt="Clayverest" className="h-10" />
       </header>
 
-      {/* Contenu principal */}
       <main className="flex-1 px-8 py-4"></main>
 
-      {/* Pied de page avec montagne */}
       <footer className="relative w-full">
         <img
           src="/Everest_Gris.svg"
